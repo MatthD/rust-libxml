09f00425d0642c6f09bb080c5e016a088093c451
MatthD <matthias.dieudonne@gmail.com>
2023-09-21 10:37:30 +0200

    feat(stream) just for a save of this state on stream

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,107 @@
-extern crate xmltree;
+extern crate xml;
 
 use std::fs::File;
-use std::io::Read;
+use std::io::BufReader;
+use xml::reader::{EventReader, XmlEvent};
 
+// Fonction principale pour vérifier la bien-forme du fichier XML
+pub fn is_wellformed_from_file(path: &str) -> Result<bool, Vec<String>> {
+    let file = File::open(path).expect("Error opening XML file.");
+    let reader = BufReader::new(file);
 
-use xmltree::Element;
+    let parser = EventReader::new(reader);
+    let mut last_opened_element = String::new();
+    let mut errors: Vec<String> = Vec::new();
 
+    for event in parser {
+        let _ = process_xml_event(event, &mut last_opened_element, &mut errors);
+    }
 
-pub fn xml_read_file(path: &str) -> Result<Element, String> {
-    dbg!(&path);
-    // Open the XML file
-    let mut file = File::open(path).expect("Error opening XML file.");
+    dbg!(&errors);
 
-    // Read the content of the XML file into a string
-    let mut xml_string = String::new();
-    file.read_to_string(&mut xml_string).expect("Error reading XML file.");
-
-    // Parse the XML string
-    Ok(Element::parse(xml_string.as_bytes()).expect("Error parsing XML."))
+    if errors.is_empty() {
+        Ok(true)
+    } else {
+        Err(errors)
+    }
 }
 
+// Fonction pour traiter les événements XML
+fn process_xml_event(event: Result<XmlEvent, xml::reader::Error>, last_opened_element: &mut String, errors: &mut Vec<String>) -> Result<(), xml::reader::Error> {
+    match event {
+        Ok(XmlEvent::StartElement { name, attributes, .. }) => {
+            let element_name = name.local_name;
+            last_opened_element.clear();
+            last_opened_element.push_str(&element_name);
+
+            for attr in attributes.iter() {
+                if let Err(attr_err) = check_attribute(&attr.value, &element_name) {
+                    errors.push(attr_err);
+                }
+            }
+        }
+        Ok(XmlEvent::EndElement { name, .. }) => {
+            let element_name = name.local_name;
+            dbg!(&element_name);
+            // Check if element are correctly closed by checking last element opened, no memory needed
+            println!("last_opened_element {}, element_name {}", last_opened_element, &element_name);
+            if last_opened_element != &element_name {
+                errors.push(format!("Opened but unclosed element '{}'.", last_opened_element));
+            }
+
+        }
+        Ok(XmlEvent::Characters(text)) => {
+            if let Err(text_err) = check_text(&text) {
+                errors.push(text_err);
+            }
+        }
+        Err(e) => {
+            dbg!(e);
+        }
+        _ => {}
+    }
+    
+    // Correction : Utilisez `Ok(())` au lieu de `ok(())` pour renvoyer un résultat Ok vide
+    Ok(())
+}
+
+// unit simple functions under this
+
+
+// Fonction pour vérifier si un caractère doit être échappé
+fn should_escape(caracter: char) -> bool {
+    caracter == '&' || caracter == '<' || caracter == '>'
+}
+
+// Fonction pour vérifier si un attribut contient des caractères non échappés
+fn check_attribute(attribute: &str, element_name: &str) -> Result<(), String> {
+    if attribute.chars().any(should_escape) {
+        Err(format!("Attribute '{}' in element '{}' contains unescaped special characters: '{}'", attribute, element_name, attribute))
+    } else {
+        Ok(())
+    }
+}
+
+// Fonction pour vérifier si des caractères de texte contiennent des caractères non échappés
+fn check_text(text: &str) -> Result<(), String> {
+    if text.chars().any(should_escape) {
+        Err("Character data inside an element contains unescaped special characters.".to_string())
+    } else {
+        Ok(())
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
 
     #[test]
-    fn test_xml_read_file_wellformed_file() {
-
-        // Read the XML file using the xml_read_file function
-        // We dont cover the case the file does not exist and return none its another test..
-        if let Ok(root_element) = xml_read_file("./tests/data/test-default.xml"){
-            // Check that the root element is correct
-            assert_eq!(root_element.name, "xpath");
-            let infosdust = root_element.get_child("to").unwrap().get_child("my").unwrap().get_child("infos").unwrap().get_text().unwrap();
-            // let infodust_element = root.get_element("to/my/infodust").expect("Element 'infodust' not found.");
-            assert_eq!(infosdust, "trezaq");
-        }
-
-    }
-
-    #[test]
-    #[should_panic(expected = "Error opening XML file.")]
-    fn test_xml_read_file_not_existing_file() {
-        // Try to read a not wellformed XML file that does not exist
-        if let Err(err) = xml_read_file("tests/data/non_existent_file.xml") {
-            panic!("{}", err);
+    fn test_is_wellformed_from_file_wellformed_file() {
+        // Test with a well-formed XML file
+        if let Err(err) = is_wellformed_from_file("./tests/data/test-not-wellformed.xml") {
+            dbg!("{}", &err);
+            // panic!("{}", &err.join("\n"));
         };
-    }
-    #[test]
-    #[should_panic(expected = "Error parsing XML.")]
-    fn test_xml_read_file_not_wellformed_file() {
-        // Try to read a not wellformed XML file that does not exist
-        if let Err(err)  = xml_read_file("tests/data/test-not-wellformed.xml") {
-            panic!("{}", err);
-        }
+
     }
 }
